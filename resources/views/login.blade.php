713543d73340193e691cbc0f315f76ac84e890e0
Dicky Ari <dickyari.setiawan.dev@gmail.com>
2024-12-17 18:09:41 +0700

    Update Revision

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,75 +1,75 @@
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Absensi Pegawai</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Responsive Admin Dashboard Template">
+    <meta name="keywords" content="admin,dashboard">
+    <meta name="author" content="stacks">
+    <!-- The above 6 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    
+    <!-- Title -->
+    <title>Neptune - Responsive Admin Dashboard Template</title>
 
-    {{-- Bootstrap CSS --}}
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-    <script type="text/javascript" src="{{ asset('assets-landing/js/instascan.min.js') }}"></script>
+    <!-- Styles -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet">
+    <link href="{{ asset('assets/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/plugins/perfectscroll/perfect-scrollbar.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/plugins/pace/pace.css') }}" rel="stylesheet">
+
+    
+    <!-- Theme Styles -->
+    <link href="{{ asset('assets/css/main.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/custom.css') }}" rel="stylesheet">
+
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/neptune.png') }}" />
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/neptune.png') }}" />
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
-
 <body>
+    <div class="app app-auth-sign-in align-content-stretch d-flex flex-wrap justify-content-end">
+        <div class="app-auth-background">
 
-    <div class="container-fluid">
-        <section class="vh-100" style="background-color: #508bfc;">
-            <div class="container py-5 h-100">
-                <div class="row d-flex justify-content-center align-items-center h-100">
-                    <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                        <div class="card shadow-2-strong" style="border-radius: 1rem;">
-                            <div class="card-body p-5 text-center">
-
-                                <h3 class="mb-5">Sign in</h3>
-                                @if (session('error'))
-                                    <div class="alert alert-danger">
-                                        {{ session('error') }}
-                                    </div>
-                                @endif
-                                @if (session('success'))
-                                    <div class="alert alert-success">
-                                        {{ session('success') }}
-                                    </div>
-                                @endif
-                                @if ($errors->any())
-                                    <div class="alert alert-danger">
-                                        @foreach ($errors->all() as $error)
-                                            {{ $error }}
-                                        @endforeach
-                                    </div>
-                                @endif
-                                <form action="{{ route('loginPost') }}" method="post">
-                                    @csrf
-                                    <div data-mdb-input-init class="form-outline mb-4">
-                                        <input type="email" name="email" id="typeEmailX-2"
-                                            class="form-control form-control-lg" />
-                                        <label class="form-label" for="typeEmailX-2">Email</label>
-                                    </div>
-
-                                    <div data-mdb-input-init class="form-outline mb-4">
-                                        <input type="password" name="password" id="typePasswordX-2"
-                                            class="form-control form-control-lg" />
-                                        <label class="form-label" for="typePasswordX-2">Password</label>
-                                    </div>
-
-                                    <button data-mdb-button-init data-mdb-ripple-init
-                                        class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        </div>
+        <form action="{{ route('loginPost') }}" method="post">
+            @csrf
+            <div class="app-auth-container">
+                <div class="logo">
+                    <a href="{{ route('login') }}">Absensi Pegawai Kantor Camat SIAK HULU</a>
             </div>
-        </section>
+            <p class="auth-description">Silahkan login ke akun anda dan lanjutkan ke dashboard</p>
+
+            <div class="auth-credentials m-b-xxl">
+                <label for="email" class="form-label">Email address</label>
+                <input type="email" name="email" required class="form-control m-b-md" id="email" aria-describedby="email" placeholder="example@gmail.com">
+                
+                <label for="password" class="form-label">Password</label>
+                <input type="password" name="password" required class="form-control" id="password" aria-describedby="password" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;">
+            </div>
+
+            <div class="auth-submit">
+                <button type="submit" class="btn btn-primary">Sign In</button>
+            </div>
+        </form>
     </div>
-
-
-    {{-- Bootstrap JS --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
+    
+    <!-- Javascripts -->
+    <script src="{{ asset('assets/plugins/jquery/jquery-3.5.1.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/perfectscroll/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/pace/pace.min.js') }}"></script>
+    <script src="{{ asset('assets/js/main.min.js') }}"></script>
+    <script src="{{ asset('assets/js/custom.js') }}"></script>
 </body>
-
 </html>
